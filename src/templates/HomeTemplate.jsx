4c8335834fb221af1fb243eceac3f99b7c4abb74
ThoaiNguyen2205 <thoainguyen22cn11@gmail.com>
2023-06-19 11:57:24 +0700

    deploy

--- src/templates/HomeTemplate.jsx
+++ src/templates/HomeTemplate.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Navigation from "../components/Navigation/Navigation";
